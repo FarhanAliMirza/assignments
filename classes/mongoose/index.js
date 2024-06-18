1497ff7d22c7214d1527541df7bea2cab9eae5c3
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2024-06-19 02:01:05 +0530

    removed class notes

--- classes/mongoose/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require ("mongoose");
-
-mongoose.connect("mongodb+srv://farhanalimirza19:lA5XLb1GJa0ZwCoK@cluster0.cs5xmon.mongodb.net/usersappnew")
-
-const User = mongoose.model('Users', { name: String, email: String, password: String });
-
-const user = new User({ name: 'Farhan', email: 'farhan@gmail.com', password: '123456' });
-user.save()
