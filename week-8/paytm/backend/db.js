21fe9ad8fbeafe918b28a367a495b259379d969a
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2024-08-27 16:04:14 +0530

    done 8.2 user routes

--- week-8/paytm/backend/db.js
+++ week-8/paytm/backend/db.js
@@ -1,15 +1,40 @@
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb+srv://farhanalimirza19:lA5XLb1GJa0ZwCoK@cluster0.cs5xmon.mongodb.net/paytm");
-const {Schema} = mongoose;
 
-const userSchema = new Schema({
-    firstName: {String, required: true},
-    lastName: {String, required: true},
-    username: {String, required: true},
-    password: {String, required: true},
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
 });
 
+// Create a model from the schema
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = {
+	User
+};
--- week-8/paytm/backend/routes/user.js
+++ week-8/paytm/backend/routes/user.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const zod = require("zod");
-const {User} = require("../db");
+const { User } = require("../db");
 const JWT_SECRET = require("../config");
 const jwt = require("jsonwebtoken");
-const {authMiddleware} = require("../middleware");
-
+const { authMiddleware } = require("../middleware");
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjQ3NTAxODF9.6MVrpw7QYVGMrwN_FpoZYc0bUTjFXdgxsx0TXzyq0r0
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NmNkOWQ1OTQxNTRkMGZiZGEyNzVkY2QiLCJpYXQiOjE3MjQ3NTExOTN9.kA1TSE4htC3mSX-EV8TvfAUw7_PtjBmwvOU_o6XVdAE
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjQ3NTEyNTB9.QDrQnSH6T5dVAmGIFIlpkS12q_nYIVgHsfy6STwPRIc
 const router = express.Router();
 
+//all schemas
 const signupSchema = zod.object({
   firstName: zod.string(),
   lastName: zod.string(),
@@ -14,13 +17,18 @@ const signupSchema = zod.object({
   password: zod.string(),
 });
 
-const signinSchema = zod.object({  
+const signinSchema = zod.object({
   username: zod.string().email(),
   password: zod.string(),
-  firstName: zod.string(),
-  lastName: zod.string(),
 });
 
+const updateSchema = zod.object({
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+  password: zod.string().optional(),
+});
+
+//all routes
 router.post("/signup", async (req, res) => {
   const body = req.body;
   const success = signupSchema.safeParse(body);
@@ -28,8 +36,8 @@ router.post("/signup", async (req, res) => {
     return res.status(411).json({ message: "Invalid data" });
   }
 
-  const user = User.findOne({ username: body.username });
-  if (user._id) {
+  const user = await User.findOne({ username: body.username });
+  if (user) {
     return res.status(411).json({ message: "User already exists" });
   }
 
@@ -41,11 +49,65 @@ router.post("/signup", async (req, res) => {
 router.post("/signin", async (req, res) => {
   const body = req.body;
   const success = signinSchema.safeParse(body);
-    if (!success) {
-        return res.status(411).json({ message: "Error while loggin in" });
-    }
-  const user = User.findOne({ username: body.username });
+  if (!success) {
+    return res.status(411).json({ message: "Error while loggin in" });
+  }
+  const existingUser = await User.findOne({
+    username: body.username,
+    password: body.password,
+  });
 
+  if (existingUser) {
+    const token = jwt.sign({ userId: existingUser._id }, JWT_SECRET);
+    res.json({ message: "User logged in successfully", token: token });
+  } else return res.status(411).json({ message: "Error while logging in" });
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateSchema.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+
+  await User.updateOne(
+    {
+      _id: req.userId,
+    },
+    req.body
+  );
+
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+router.get("/bulk", authMiddleware, async (req, res) => {
+  const filter = req.query.filter || "";
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
 });
 
 module.exports = router;
